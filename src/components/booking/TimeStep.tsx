d39bcb991ff46cf9e2e6a7f3cde6b91e239bbaf0
adisakkk <ubonubon6532@gmail.com>
2025-12-12 18:38:10 +0700

    forcebutton

--- src/components/booking/TimeStep.tsx
+++ src/components/booking/TimeStep.tsx
@@ -138,11 +138,17 @@ export function TimeStep({
                 )}
             </div>
 
+            {/* Spacer when button is fixed */}
+            {selectedSlot && <div className="h-24" />}
+
+            {/* Fixed bottom button when slot is selected */}
             {selectedSlot && (
-                <div className="mt-6 fade-in">
-                    <Button onClick={onContinue} isLoading={isValidating}>
-                        {isValidating ? 'Checking availability...' : 'Continue'}
-                    </Button>
+                <div className="fixed bottom-0 left-0 right-0 p-4 bg-white border-t border-gray-200 shadow-[0_-4px_20px_rgba(0,0,0,0.1)] z-50 fade-in">
+                    <div className="max-w-xl mx-auto">
+                        <Button onClick={onContinue} isLoading={isValidating}>
+                            {isValidating ? 'Checking availability...' : 'Continue'}
+                        </Button>
+                    </div>
                 </div>
             )}
         </section>
